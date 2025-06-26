897bfeff55421316b0abd225183bea4133bc7300
Stagiaire 6 Bayonne <stagiaire6-bayonne@afec.fr>
2025-06-26 15:38:24 +0200

    there's no way i forgot to allow webp format for thumbnails, surely

--- backend/middlewares/multerMiddleware.js
+++ backend/middlewares/multerMiddleware.js
@@ -25,7 +25,8 @@ const fileFilter = (req, file, cb) => {
     file.mimetype === 'video/webm' ||
     file.mimetype === 'image/jpeg' || 
     file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg'
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/webp'
   ) {
     cb(null, true);
   } else {
